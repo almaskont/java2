1b995856c73a9128f6089146a22b6d97784a02a1
Almas <almaskon@gmail.com>
2022-02-18 10:19:42 +0600

    changed second task

--- src/main/java/homework3/Phonebook.java
+++ src/main/java/homework3/Phonebook.java
@@ -1,21 +1,24 @@
 package homework3;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Phonebook {
-    HashMap<String, String> phoneBook = new HashMap<>();
+    HashMap<String, ArrayList<String>> phoneBook = new HashMap<>();
 
     public void add(String surname, String number) {
-        phoneBook.put(number, surname);
+        if(phoneBook.containsKey(surname)){
+            phoneBook.get(surname).add(number);
+        } else {
+            ArrayList<String> numbers = new ArrayList<>();
+            numbers.add(number);
+            phoneBook.put(surname, numbers);
+        }
     }
 
     public void get(String surname) {
-        if (phoneBook.containsValue(surname)) {
-            for (String contact : phoneBook.keySet()) {
-                if (phoneBook.get(contact).equals(surname)) {
-                    System.out.println(surname + ": " + contact);
-                }
-            }
+        if (phoneBook.containsKey(surname)) {
+            System.out.println(surname + ": " + phoneBook.get(surname).toString());
         } else {
             System.out.println("No contact");
         }
