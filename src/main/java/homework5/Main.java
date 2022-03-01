6d2c0bdf5aad97ccc1d1e7e2c99e24b58ed2667b
Almas <almaskon@gmail.com>
2022-03-01 21:43:28 +0600

    added homework5

---
+++ src/main/java/homework5/Main.java
@@ -0,0 +1,60 @@
+package homework5;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int size = 100000000;
+        System.out.println("Without thread");
+        first(size);
+        System.out.println("With a thread");
+        second(size);
+    }
+
+    public static void first(int size) {
+        float[] arr = new float[size];
+        Arrays.fill(arr, 1);
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < size; i++){
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    public static void second(int size) {
+        int h = size/2;
+        float[] arr = new float[size];
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+
+        Arrays.fill(arr, 1);
+
+        long a = System.currentTimeMillis();
+
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+        System.out.println(System.currentTimeMillis() - a);
+    }
+}
